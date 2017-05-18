349afd1db08e274a8080776a2e93bc1f9b590a82
Jeff Laflamme <jflaflamme@gmail.com>
2017-05-18 15:17:51 +0700

    modified clone of nodejs-tk102 to support h02

--- h02.js
+++ h02.js
@@ -1,21 +1,24 @@
 /*
-Name:         tk102
+Name:         h02
 Description:  TK102 GPS server for Node.js
 Author:       Franklin van de Meent (https://frankl.in)
-Source:       https://github.com/fvdm/nodejs-tk102
-Feedback:     https://github.com/fvdm/nodejs-tk102/issues
+Source:       https://github.com/fvdm/nodejs-h02
+Feedback:     https://github.com/fvdm/nodejs-h02/issues
 License:      Unlicense / Public Domain (see UNLICENSE file)
-              (https://github.com/fvdm/nodejs-tk102/raw/master/UNLICENSE)
+              (https://github.com/fvdm/nodejs-h02/raw/master/UNLICENSE)
 */
 
 var net = require ('net');
 var EventEmitter = require ('events') .EventEmitter;
-var tk102 = new EventEmitter ();
+var h02 = new EventEmitter ();
 
 // device data
 var specs = [
   function (raw) {
     // 1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123
+    // *HQ,4209950057,V1,035522,A,1321.1457,N,10351.0798,E,000.00,000,240317,BFFFFBFF,456,06,0,0,6#
+    // *HQ,4209950057,#
+    // *HQ,4209950057,V19,035522,A,1321.1457,N,10351.0798,E,000.00,000,240317,,,8985506081694161822F,BFFFFBFF# 
     var result = null;
     var str = [];
     var datetime = '';
@@ -25,9 +28,9 @@ var specs = [
     try {
       raw = raw.trim ();
       str = raw.split (',');
-
-      if (str.length === 18 && str [2] === 'GPRMC') {
-        datetime = str [0] .replace (/([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/, function (s, y, m, d, h, i) {
+      
+	if (str.length === 18 && str [2] === 'V1') {
+        datetime = str [11] .replace (/([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/, function (s, y, m, d, h, i) {
           return '20' + y + '-' + m + '-' + d + ' ' + h + ':' + i;
         });
 
@@ -35,9 +38,11 @@ var specs = [
           return '20' + y + '-' + m + '-' + d;
         });
 
-        gpstime = str [3] .replace (/([0-9]{2})([0-9]{2})([0-9]{2})\.([0-9]{3})/, function (s0, h, i, s, ms) {
-          return h + ':' + i + ':' + s + '.' + ms;
+        gpstime = str [3] .replace (/([0-9]{2})([0-9]{2})([0-9]{2})/, function (s0, h, i, s) {
+          return h + ':' + i + ':' + s;
         });
+
+	allMasks = h02.getBitMasks( str [12] );
 
         result = {
           raw: raw,
@@ -46,25 +51,33 @@ var specs = [
           gps: {
             date: gpsdate,
             time: gpstime,
-            signal: str [15] === 'F' ? 'full' : 'low',
-            fix: str [4] === 'A' ? 'active' : 'invalid'
+            //signal: str [15] === 'F' ? 'full' : 'low', // from binary TODO
+            fix: str [4] === 'A' ? 'active' : 'invalid',
+            batt: str [14]
           },
           geo: {
-            latitude: tk102.fixGeo (str [5], str [6]),
-            longitude: tk102.fixGeo (str [7], str [8]),
+            latitude: h02.fixGeo (str [5], str [6]),
+            longitude: h02.fixGeo (str [7], str [8]),
             bearing: parseInt (str [10], 10)
           },
           speed: {
             knots: Math.round (str [9] * 1000) / 1000,
-            kmh: Math.round (str [9] * 1.852 * 1000) / 1000,
-            mph: Math.round (str [9] * 1.151 * 1000) / 1000
+            kmh: Math.round (str [10] * 1.852 * 1000) / 1000,
+            mph: Math.round (str [10] * 1.151 * 1000) / 1000
           },
-          imei: str [16] .replace ('imei:', ''),
-          checksum: tk102.checksum (raw)
+	  status: {
+	    'retention': {'power_cut': false},
+            'gps': {'vibration': false, 'power_cut': false, 'shock': false, 'low_battery': allMasks[1][7]},
+            'vehicle': {'armed': false, 'ac': false},
+            'alarm': {'sos': false, 'speed': false, 'drop': allMasks[3][1] === false ? true : false } 
+	  },
+          rawBitMasks: allMasks,
+          imei: str [1],
+          //checksum: h02.checksum (raw) // TODO No checksum info
         };
       }
     } catch (e) {
-      result = null;
+      result = e
     }
 
     return result;
@@ -72,7 +85,7 @@ var specs = [
 ];
 
 // defaults
-tk102.settings = {
+h02.settings = {
   ip: '0.0.0.0',
   port: 0,
   connections: 10,
@@ -81,9 +94,9 @@ tk102.settings = {
 
 
 // Emit event
-tk102.event = function (name, value) {
-  tk102.emit (name, value);
-  tk102.emit ('log', name, value);
+h02.event = function (name, value) {
+  h02.emit (name, value);
+  h02.emit ('log', name, value);
 };
 
 // Catch uncaught exceptions (server kill)
@@ -92,47 +105,47 @@ process.on ('uncaughtException', function (err) {
 
   error.error = err;
   console.log (error);
-  tk102.event ('error', error);
+  h02.event ('error', error);
 });
 
 // Create server
-tk102.createServer = function (vars) {
+h02.createServer = function (vars) {
   var key;
 
   // override settings
   if (typeof vars === 'object' && Object.keys (vars) .length >= 1) {
     for (key in vars) {
-      tk102.settings [key] = vars [key];
+      h02.settings [key] = vars [key];
     }
   }
 
   // start server
-  tk102.server = net.createServer ();
+  h02.server = net.createServer ();
 
   // maximum number of slots
-  tk102.server.maxConnections = tk102.settings.connections;
+  h02.server.maxConnections = h02.settings.connections;
 
   // server started
-  tk102.server.on ('listening', function () {
-    tk102.event ('listening', tk102.server.address ());
+  h02.server.on ('listening', function () {
+    h02.event ('listening', h02.server.address ());
   });
 
   // inbound connection
-  tk102.server.on ('connection', function (socket) {
-    var connection = tk102.server.address ();
+  h02.server.on ('connection', function (socket) {
+    var connection = h02.server.address ();
 
     connection.remoteAddress = socket.remoteAddress;
     connection.remotePort = socket.remotePort;
 
-    tk102.event ('connection', connection);
+    h02.event ('connection', connection);
     socket.setEncoding ('utf8');
 
-    if (tk102.settings.timeout > 0) {
-      socket.setTimeout (parseInt (tk102.settings.timeout * 1000, 10));
+    if (h02.settings.timeout > 0) {
+      socket.setTimeout (parseInt (h02.settings.timeout * 1000, 10));
     }
 
     socket.on ('timeout', function () {
-      tk102.event ('timeout', connection);
+      h02.event ('timeout', connection);
       socket.destroy ();
     });
 
@@ -141,27 +154,27 @@ tk102.createServer = function (vars) {
       var err = null;
 
       data = data.trim ();
-      tk102.event ('data', data);
+      h02.event ('data', data);
 
       if (data !== '') {
-        gps = tk102.parse (data);
+        gps = h02.parse (data);
 
         if (gps) {
-          tk102.event ('track', gps);
+          h02.event ('track', {'data': gps, 'socket': socket});
         } else {
           err = new Error ('Cannot parse GPS data from device');
           err.reason = err.message;
           err.input = data;
           err.connection = connection;
 
-          tk102.event ('fail', err);
+          h02.event ('fail', err);
         }
       }
     });
 
     socket.on ('close', function (hadError) {
       connection.hadError = hadError;
-      tk102.event ('disconnect', connection);
+      h02.event ('disconnect', connection);
     });
 
     // error
@@ -170,13 +183,13 @@ tk102.createServer = function (vars) {
 
       err.reason = error.message;
       err.socket = socket;
-      err.settings = tk102.settings;
+      err.settings = h02.settings;
 
-      tk102.event ('error', err);
+      h02.event ('error', err);
     });
   });
 
-  tk102.server.on ('error', function (error) {
+  h02.server.on ('error', function (error) {
     var err = new Error ('Server error');
 
     if (error === 'EADDRNOTAVAIL') {
@@ -184,29 +197,29 @@ tk102.createServer = function (vars) {
     }
 
     err.reason = error.message;
-    err.input = tk102.settings;
+    err.input = h02.settings;
 
-    tk102.event ('error', err);
+    h02.event ('error', err);
   });
 
   // Start listening
-  tk102.server.listen (tk102.settings.port, tk102.settings.ip);
+  h02.server.listen (h02.settings.port, h02.settings.ip);
 
-  return tk102;
+  return h02;
 };
 
 // Graceful close server
-tk102.closeServer = function (callback) {
-  if (!tk102.server) {
+h02.closeServer = function (callback) {
+  if (!h02.server) {
     callback (new Error ('server not started'));
     return;
   }
 
-  tk102.server.close (callback);
+  h02.server.close (callback);
 };
 
 // Parse GPRMC string
-tk102.parse = function (raw) {
+h02.parse = function (raw) {
   var data = null;
   var i = 0;
 
@@ -219,18 +232,36 @@ tk102.parse = function (raw) {
 };
 
 // Clean geo positions, with 6 decimals
-tk102.fixGeo = function (one, two) {
+h02.fixGeo = function (one, two) {
   var minutes = one.substr (-7, 7);
   var degrees = parseInt (one.replace (minutes, ''), 10);
 
   one = degrees + (minutes / 60);
   one = parseFloat ((two === 'S' || two === 'W' ? '-' : '') + one);
-
   return Math.round (one * 1000000) / 1000000;
 };
 
+h02.getBitMasks = function (hex) {
+      theByte = 0, theBitMask = {}, theBitMaskArray = [];
+      for (var i = hex.length ; i > 0; i-=2) {
+        var v = parseInt(hex.substr(i-2, 2), 16);
+        theBitMaskArray = v.toString(2).split('');
+	theBitMask[theByte] = {};
+	for ( var j = 0; j < theBitMaskArray.length; j++ ) {
+         if ( theBitMaskArray[j] === '0' ) {
+	  // non sense but 0 means positive (see h02 doc)
+          theBitMask[theByte][j] = true;
+         } else { 
+          theBitMask[theByte][j] = false;
+         }
+        } 
+        theByte++;
+      }
+      return theBitMask;
+}
+
 // Check checksum in raw string
-tk102.checksum = function (raw) {
+h02.checksum = function (raw) {
   var str = raw.trim () .split (/[,*#]/);
   var strsum = parseInt (str [15], 10);
   var strchk = str.slice (2, 15) .join (',');
@@ -246,4 +277,4 @@ tk102.checksum = function (raw) {
 };
 
 // ready
-module.exports = tk102;
+module.exports = h02;
